138f796d8ac43c08cf9836b8f0e722ce28ceba6a
0iak <159968766+0iak@users.noreply.github.com>
2024-03-01 21:02:50 -0300

    Add files via upload

--- q16.js
+++ q16.js
@@ -1,13 +1,28 @@
 import { question } from "readline-sync"
 
-situacao = "prova final"
-
 function aprovadoounao(not1,not2){
     const media = (not1 + not2)/2
     if (media >=7){
         return 'aprovado'
-    }if(media<7){
-        return `prova final`
+    }else if(media>=5 && media<7){
+        return console.log(`${pf(media)}`)
+        
+        function pf(med){
+        console.log(`................
+        voce ficou de prova final
+        .......................`)
+
+        const nota_pf = get_number('digite sua nota final: ')
+
+        const nota_final = (nota_pf + media)/2
+
+        if(nota_final>=5){
+            return 'aprovado'
+        }else{
+            return 'reprovado'
+        }}
+    }else{
+        return 'reprovado'
     }
 }
 
@@ -25,6 +40,8 @@ function main(){
     const nota2 = get_number('digite sua nota 2: ')
     
     const situacao = aprovadoounao(nota1,nota2)
+
+    console.log(`${situacao}`)
 
 }
 
